92a6d95b1c78ee5fddcf6fd6f41e63debc98448b
yangdong <yangdong@guotailimin.com>
2021-11-10 10:27:37 +0800

    feat: add pull.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,25 +1,28 @@
-const fs = require('fs')
-const path = require('path')
-const util = require('util')
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
 
-const sub_app_path = path.resolve();
+const subApp = fs.readdirSync(path.resolve(__dirname, "../subapp")).filter(item => item !== ".git");
 
-const sub_apps = fs.readdirSync(sub_app_path).filter(i => /^master|subapp/.test(i));
+const apps = ["main", ...subApp];
 
-console.log(`即将进入所有模块并打包项目：${JSON.stringify(sub_apps)} ing...`)
+console.log(`即将进入所有模块并打包项目：${JSON.stringify(apps)} ing...`);
 
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(require("child_process").exec);
 function build() {
-    sub_apps.forEach(async i => {
-        console.log(`[${i}] 开始打包,耗时较久请耐心等待...`)
-        const { stdout, stderr } = await exec('npm run build', { cwd: path.resolve(i) });
-        console.log(i, 'success', stdout)
-        console.error(i, 'error', stderr)
-    });
-};
+	apps.forEach(async i => {
+		if (i !== "main") {
+			i = "subapp/" + i;
+		}
+		console.log(`[${i}] 开始打包,耗时较久请耐心等待...`);
+		const { stdout, stderr } = await exec("yarn build", { cwd: path.resolve(i) });
+		console.log(i, "success", stdout);
+		console.error(i, "error", stderr);
+	});
+}
 build();
 
-process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-    // application specific logging, throwing an error, or other logic here
+process.on("unhandledRejection", (reason, p) => {
+	console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+	// application specific logging, throwing an error, or other logic here
 });
---
+++ scripts/pull.sh
@@ -0,0 +1,6 @@
+cd main;
+echo '开始拉取基座应用';
+git pull;
+cd ../subapp;
+echo '开始拉取子应用';
+git pull;
