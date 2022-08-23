8717faa2a7af2892293b86b54ef00e1a8e380cb9
yangdong <yangdong@guotailimin.com>
2022-08-23 17:54:04 +0800

    feat:  modified scripts

--- scripts/clone.sh
+++
@@ -1,5 +0,0 @@
-# 基座应用
-git clone git@github.com:Jesse121/main.git
-
-# 子应用
-git clone git@github.com:Jesse121/subapp.git
--- scripts/pull.sh
+++
@@ -1,6 +0,0 @@
-cd main;
-echo '开始拉取基座应用';
-git pull;
-cd ../subapp;
-echo '开始拉取子应用';
-git pull;
